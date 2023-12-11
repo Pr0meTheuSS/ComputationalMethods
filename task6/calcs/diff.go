b8340c90642f1101858a61d177273e237ac9657d
Yriy Olimpiev <y.olimpiev@g.nsu.ru>
2023-12-11 14:15:47 +0700

    Passed.

--- task6/calcs/diff.go
+++ task6/calcs/diff.go
@@ -1,14 +1,24 @@
 package calcs
 
-// Решение дифференциального уравнения y'(x) = -y(x)
-func differentialEquation(y float64) float64 {
-	return -y
+import "math"
+
+func solution(x float64) float64 {
+	return 1.0 / math.Exp(x)
 }
 
-func FirstOrderDiff(x []float64, y0 float64) []float64 {
-	n := len(x)
+func solution1(x float64) float64 {
+	return math.Exp(x)*0.5*(math.Sin(x)+math.Cos(x)) - 0.5
+}
 
-	h := (x[n-1] - x[0]) / float64(n)
+func FirstOrderDiff(x []float64, y0, h float64, g func(x, y float64) float64) []float64 {
+	/*
+		y(x+h) - y(x)
+		-------------  = g(x)
+			  h
+
+		y[i+1] = g(x) *h + y[i]
+	*/
+	n := len(x)
 	y := make([]float64, n)
 
 	// Начальные условия
@@ -16,88 +26,90 @@ func FirstOrderDiff(x []float64, y0 float64) []float64 {
 
 	// Решение уравнения с разностной схемой
 	for i := 0; i < n-1; i++ {
-		y[i+1] = y[i] + h*differentialEquation(y[i])
+		y[i+1] = y[i] + h*g(x[i], y[i])
 	}
 
 	return y
 }
 
 // Функция для разностной схемы второго порядка аппроксимации
-func SecondOrderDiff(x []float64, y0 float64, f func(float64) float64) []float64 {
+func SecondOrderDiff(x []float64, y0, h float64, g func(x, y float64) float64) []float64 {
+	/*
+
+		y(x+h) - y(x-h)     g(x+h) + g(x-h)         |
+		-------------   -   ----------------  = 0   |  * 2h
+			  2h                   2                |
+
+
+		y(x+h) = h(g(x+h) + g(x-h)) + y(x-h)
+
+
+		y[i+1] = h * (g(x[i+1]) + g(x[i-1])) + y[i-1]
+
+
+
+		y(x+h) - y(x-h)    				 |
+		-------------   -  g(x)  =   0   |  * 2h
+			  2h                         |
+
+			  y[i+1] = y[i-1] +2*h*g(x)
+
+
+	*/
 	n := len(x)
-	h := (x[n-1] - x[0]) / float64(n-1)
 	y := make([]float64, n)
 
 	y[0] = y0
+	y[1] = y[0] + h*g(x[0], y[0])
 
-	for i := 0; i < n-1; i++ {
-		y[i+1] = y[i] + h*f(x[i]) + (h*h/2)*f(x[i])
+	for i := 1; i < n-1; i++ {
+		y[i+1] = (1.0-h)*y[i] + (h/2.0)*(y[i-1]-y[i])
+	}
+
+	return y
+}
+
+// Функция для разностной схемы второго порядка аппроксимации c g(x)
+func SecondOrderDiffCos(x []float64, y0, h float64, g func(x, y float64) float64) []float64 {
+	n := len(x)
+	y := make([]float64, n)
+
+	y[0] = y0
+	y[1] = y[0] + h*g(x[0], y[0])
+
+	for i := 1; i < n-1; i++ {
+		y[i+1] = h*(g(x[i+1], y[i+1])+g(x[i-1], y[i-1])) + y[i-1]
 	}
 
 	return y
 }
 
 // Функция для разностной схемы четвертого порядка аппроксимации
-func FourthOrderDiff(x []float64, y0 float64, f func(float64) float64) []float64 {
+func FourthOrderDiff(x []float64, y0, h float64) []float64 {
 	n := len(x)
-
-	h := (x[n-1] - x[0]) / float64(n)
 	y := make([]float64, n)
 
 	y[0] = y0
+	y[1] = solution(x[1])
 
-	// Решение уравнения с разностной схемой
-	for i := 0; i < n-1; i++ {
-		k1 := h * differentialEquation(y[i])
-		k2 := h * differentialEquation(y[i]+k1/2)
-		k3 := h * differentialEquation(y[i]+k2/2)
-		k4 := h * differentialEquation(y[i]+k3)
-
-		y[i+1] = y[i] + (k1+2*k2+2*k3+k4)/6
+	for i := 1; i < n-1; i++ {
+		y[i+1] = (y[i-1]*(3-h) - 4*h*y[i]) / (3.0 + h)
 	}
 
 	return y
 }
 
-// Центральная разностная схема второго порядка (k=2)
-func SecondOrderCentralDifference(x []float64, h float64, f func(float64) float64) []float64 {
+// Функция для разностной схемы четвертого порядка аппроксимации
+func FourthOrderDiffWithG(x []float64, y0, h float64, g func(x, y float64) float64) []float64 {
 	n := len(x)
-	y := make([]float64, n)
-
-	// Начальные условия
-	y[0] = 0
-
-	// Решение уравнения с разностной схемой
-	for i := 0; i < n-1; i++ {
-		y[i+1] = y[i] + h*f(y[i]) + (h*h/2)*f(y[i])
-	}
-
-	// Главный член невязки для второй схемы
-	// residual := make([]float64, n)
-	// for i := 1; i < n-1; i++ {
-	// 	residual[i] = y[i] - math.Exp(x[i])*math.Cos(x[i])
-	// }
-
-	return y
-}
-
-// Центральная разностная схема четвертого порядка (k=4)
-func FourthOrderCentralDifference(x []float64, y0 float64) []float64 {
-	n := len(x)
-
-	h := (x[n-1] - x[0]) / float64(n)
 	y := make([]float64, n)
 
 	y[0] = y0
-	for i := 2; i < n-2; i++ {
-		y[i] = (-x[i+2] + 8*x[i+1] - 8*x[i-1] + x[i-2]) / (12 * h)
-	}
+	y[1] = y[0] + h*g(x[0], y[0])
 
-	// Главный член невязки для второй схемы
-	// residual := make([]float64, n)
-	// for i := 2; i < n-2; i++ {
-	// 	residual[i] = y[i] - math.Exp(x[i])*math.Cos(x[i])
-	// }
+	for i := 1; i < n-1; i++ {
+		y[i+1] = 2*h*(g(x[i-1], y[i-1])/6.+2*g(x[i], y[i])/3+g(x[i+1], y[i+1])/6) + y[i-1]
+	}
 
 	return y
 }
--- task6/tests/main_test.go
+++ task6/tests/main_test.go
@@ -11,11 +11,11 @@ func solution(x float64) float64 {
 }
 
 const (
-	eps = 1e-2
+	eps = 1e-4
 )
 
 func TestCalcDiffFirstOrder(t *testing.T) {
-	step := 0.000001
+	step := 0.0001
 	left := 0.0
 	right := 5.0
 	n := (int64)((right - left) / step)
@@ -25,7 +25,7 @@ func TestCalcDiffFirstOrder(t *testing.T) {
 		x[i] = left + float64(i)*step
 	}
 
-	res := calcs.FirstOrderDiff(x, 1.0)
+	res := calcs.FirstOrderDiff(x, 1.0, step, func(x, y float64) float64 { return -y })
 	for i, r := range res {
 		if math.Abs(solution(x[i])-r) > eps {
 			t.Fatalf("Неверное решение дифф. уравнения. Ожидается %v, получено %v", solution(x[i]), r)
@@ -34,7 +34,7 @@ func TestCalcDiffFirstOrder(t *testing.T) {
 }
 
 func TestCalcDiffSecondOrder(t *testing.T) {
-	step := 0.001
+	step := 0.01
 	left := 0.0
 	right := 5.0
 	n := (int64)((right - left) / step)
@@ -44,16 +44,16 @@ func TestCalcDiffSecondOrder(t *testing.T) {
 		x[i] = left + float64(i)*step
 	}
 
-	res := calcs.SecondOrderDiff(x, 1.0, func(x float64) float64 { return -x })
+	res := calcs.SecondOrderDiff(x, 1.0, step, func(x, y float64) float64 { return -y })
 	for i, r := range res {
 		if math.Abs(solution(x[i])-r) > eps {
-			t.Fatalf("Неверное решение дифф. уравнения. Ожидается %v, получено %v", solution(x[i]), r)
+			t.Fatalf("Неверное решение дифф. уравнения. Ожидается %v, получено %v. Итерация: %d", solution(x[i]), r, i)
 		}
 	}
 }
 
 func TestCalcDiffFourthOrder(t *testing.T) {
-	step := 0.00001
+	step := 0.1
 	left := 0.0
 	right := 5.0
 	n := (int64)((right - left) / step)
@@ -63,10 +63,10 @@ func TestCalcDiffFourthOrder(t *testing.T) {
 		x[i] = left + float64(i)*step
 	}
 
-	res := calcs.FourthOrderDiff(x, 1.0, func(x float64) float64 { return -x })
+	res := calcs.FourthOrderDiff(x, 1.0, step)
 	for i, r := range res {
 		if math.Abs(solution(x[i])-r) > eps {
-			t.Fatalf("Неверное решение дифф. уравнения. Ожидается %v, получено %v", solution(x[i]), r)
+			t.Fatalf("Неверное решение дифф. уравнения. Ожидается %v, получено %v. Итерация: %d", solution(x[i]), r, i)
 		}
 	}
 }
@@ -76,7 +76,7 @@ func solution1(x float64) float64 {
 }
 
 func TestCalcSecondOrderCosExp(t *testing.T) {
-	step := 0.001
+	step := 0.1
 	left := 0.0
 	right := 2.5
 	n := (int64)((right - left) / step)
@@ -86,7 +86,7 @@ func TestCalcSecondOrderCosExp(t *testing.T) {
 		x[i] = left + float64(i)*step
 	}
 
-	res := calcs.SecondOrderDiff(x, 0.0, func(x float64) float64 { return math.Exp(x) * math.Cos(x) })
+	res := calcs.SecondOrderDiffCos(x, .0, step, func(x, y float64) float64 { return math.Exp(x) * math.Cos(x) })
 
 	for i, r := range res {
 		if math.Abs(solution1(x[i])-r) > eps {
@@ -95,22 +95,22 @@ func TestCalcSecondOrderCosExp(t *testing.T) {
 	}
 }
 
-// func TestCalcFourthOrderCosExp(t *testing.T) {
-// 	step := 0.001
-// 	left := 0.0
-// 	right := 2.5
-// 	n := (int64)((right - left) / step)
+func TestCalcFourthOrderCosExp(t *testing.T) {
+	step := 0.1
+	left := 0.0
+	right := 2.5
+	n := (int64)((right - left) / step)
 
-// 	x := make([]float64, n)
-// 	for i := int64(0); i < n; i++ {
-// 		x[i] = left + float64(i)*step
-// 	}
+	x := make([]float64, n)
+	for i := int64(0); i < n; i++ {
+		x[i] = left + float64(i)*step
+	}
 
-// 	res := calcs.FourthOrderDiff(x, 0.0, func(x float64) float64 { return math.Exp(x) * math.Cos(x) })
+	res := calcs.FourthOrderDiffWithG(x, 0.0, step, func(x, y float64) float64 { return math.Exp(x) * math.Cos(x) })
 
-// 	for i, r := range res {
-// 		if math.Abs(solution1(x[i])-r) > eps {
-// 			t.Fatalf("Неверное решение дифф. уравнения в x_%d. Ожидается %v, получено %v", i, solution1(x[i]), r)
-// 		}
-// 	}
-// }
+	for i, r := range res {
+		if math.Abs(solution1(x[i])-r) > eps {
+			t.Fatalf("Неверное решение дифф. уравнения в x_%d. Ожидается %v, получено %v", i, solution1(x[i]), r)
+		}
+	}
+}
